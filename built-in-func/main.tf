e99bc1653952405fb2f12685fa60db12eccb1971
Rahul-Blatz <rahulblatz002@gmail.com>
2023-12-17 08:44:37 -0500

    Built in function template

---
+++ built-in-func/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    azurerm = {
+      version = "~> 3.18"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "app_name" {
+  description = "Name of application"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment Name"
+  type        = string
+}
+
+
+resource "azurerm_resource_group" "project1" {
+  name     = upper(format("RG-%s-%s",var.app_name,var.environment))
+  location = "asia"
+}
---
+++ built-in-func/notes.md
@@ -0,0 +1,6 @@
+### Built-In Functions
+- Terraform provides some built in functions to perform some transformations and combinations.
+
+- In our case, we have used `upper` and `format` built in functions to change the case of the variables to UPPER CASE and format the strings with the variables.
+
+- There are many other prebuilt functions, check out - [built-in-function](https://www.terraform.io/docs/configuration/functions.html)
